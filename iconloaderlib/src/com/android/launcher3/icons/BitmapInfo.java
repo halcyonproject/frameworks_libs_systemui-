03448085362eafcaf49c888f8ef4a3240ecab3b7
Tommy Webb <tommy@calyxinstitute.org>
2022-12-14 12:45:28 -0500

    Multiple work profile badge colors for Launcher3
    * Add mUserHandle to BitmapInfo so that the correct user badge may be
      selected when the applyFlags is called.
    * Remove FLAG_WORK, since it only supports a single profile. (This also
      will help to make upstream changes more noticeable when porting.)
    * Restore former getBadgeForUser from Launcher3's WidgetHostViewLoader
      into BaseIconFactory here, but accept iconSize instead of badgeSize.
    
    Test: Manual: Different work profile badges have different colors
    consistently and do not change or adjust size based on unrelated
    events. Create Contacts widget/shortcut and drag to home screen.
    Drag Contacts app icon to home screen. Check Settings > Apps and
    App Info. Check Overview.
    
    Change-Id: I64f1b425eee5cc7994616050411a881d84ece99d
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- iconloaderlib/src/com/android/launcher3/icons/BitmapInfo.java
+++ iconloaderlib/src/com/android/launcher3/icons/BitmapInfo.java
@@ -20,6 +20,7 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
@@ -29,12 +30,10 @@ import com.android.launcher3.util.FlagOp;
 
 public class BitmapInfo {
 
-    static final int FLAG_WORK = 1 << 0;
     static final int FLAG_INSTANT = 1 << 1;
     static final int FLAG_CLONE = 1 << 2;
     static final int FLAG_PRIVATE = 1 << 3;
     @IntDef(flag = true, value = {
-            FLAG_WORK,
             FLAG_INSTANT,
             FLAG_CLONE,
             FLAG_PRIVATE
@@ -64,6 +63,12 @@ public class BitmapInfo {
     public @BitmapInfoFlags int flags;
     private BitmapInfo badgeInfo;
 
+    @Nullable
+    private UserHandle mUserHandle;
+
+    @Nullable
+    private Drawable mUserBadge;
+
     public BitmapInfo(Bitmap icon, int color) {
         this.icon = icon;
         this.color = color;
@@ -87,11 +92,19 @@ public class BitmapInfo {
         return result;
     }
 
+    public BitmapInfo withUser(@Nullable UserHandle user, @NonNull BaseIconFactory iconFactory) {
+        BitmapInfo result = clone();
+        result.setUser(user, iconFactory);
+        return result;
+    }
+
     protected BitmapInfo copyInternalsTo(BitmapInfo target) {
         target.mMono = mMono;
         target.mWhiteShadowLayer = mWhiteShadowLayer;
         target.flags = flags;
         target.badgeInfo = badgeInfo;
+        target.mUserHandle = mUserHandle;
+        target.mUserBadge = mUserBadge;
         return target;
     }
 
@@ -103,6 +116,15 @@ public class BitmapInfo {
     public void setMonoIcon(Bitmap mono, BaseIconFactory iconFactory) {
         mMono = mono;
         mWhiteShadowLayer = iconFactory.getWhiteShadowLayer();
+    }
+
+    public void setUser(@Nullable UserHandle user, @NonNull BaseIconFactory iconFactory) {
+        mUserHandle = user;
+        if (user != null) {
+            mUserBadge = iconFactory.getBadgeForUser(mUserHandle);
+        } else {
+            mUserBadge = null;
+        }
     }
 
     /**
@@ -126,6 +148,8 @@ public class BitmapInfo {
     public Bitmap getMono() {
         return mMono;
     }
+
+    public UserHandle getUser() { return mUserHandle; }
 
     /**
      * Creates a drawable for the provided BitmapInfo
@@ -173,10 +197,6 @@ public class BitmapInfo {
             return context.getDrawable(isThemed
                     ? R.drawable.ic_instant_app_badge_themed
                     : R.drawable.ic_instant_app_badge);
-        } else if ((flags & FLAG_WORK) != 0) {
-            return context.getDrawable(isThemed
-                    ? R.drawable.ic_work_app_badge_themed
-                    : R.drawable.ic_work_app_badge);
         } else if ((flags & FLAG_CLONE) != 0) {
             return context.getDrawable(isThemed
                     ? R.drawable.ic_clone_app_badge_themed
@@ -185,6 +205,11 @@ public class BitmapInfo {
             return context.getDrawable(isThemed
                     ? R.drawable.ic_private_profile_app_badge_themed
                     : R.drawable.ic_private_profile_app_badge);
+        } else if (mUserBadge != null) {
+            // We use a copy of the badge, or changes will affect everywhere it is used;
+            // e.g., shortcuts/widget user badges are very small, and these could affect
+            // regular launcher icons, and the other way around.
+            return mUserBadge.getConstantState().newDrawable().mutate();
         }
         return null;
     }
